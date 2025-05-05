c35180e58b9bfbf1faf806d999fe27de811363cd
Sam Kikoma <kikomasamson@gmail.com>
2025-05-05 23:10:15 +0200

    Refactor: Refactored ProductController to include GetProductsService and improve method signatures

--- src/main/java/com/example/spring/product/ProductController.java
+++ src/main/java/com/example/spring/product/ProductController.java
@@ -1,8 +1,17 @@
 package com.example.spring.product;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import com.example.spring.product.model.Product;
+import com.example.spring.product.model.ProductDTO;
+import com.example.spring.product.model.UpdateProductCommand;
+import com.example.spring.product.services.CreateProductService;
+import com.example.spring.product.services.DeleteProductService;
+import com.example.spring.product.services.GetProductService;
+import com.example.spring.product.services.GetProductsService;
+import com.example.spring.product.services.UpdateProductService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class ProductController {
@@ -10,35 +19,39 @@ public class ProductController {
     private final GetProductService getProductService;
     private final UpdateProductService updateProductService;
     private final DeleteProductService deleteProductService;
+    private final GetProductsService getProductsService;
 
-    public ProductController(CreateProductService createProductService, GetProductService getProductService, UpdateProductService updateProductService, DeleteProductService deleteProductService) {
+    public ProductController(CreateProductService createProductService, GetProductService getProductService,UpdateProductService updateProductService, DeleteProductService deleteProductService, GetProductsService getProductsService) {
         this.createProductService = createProductService;
         this.getProductService = getProductService;
         this.updateProductService = updateProductService;
         this.deleteProductService = deleteProductService;
+        this.getProductsService = getProductsService;
     }
 
-
-
-    @PostMapping
-    public ResponseEntity<String> createProduct(){
-        return createProductService.execute(null);
+    @PostMapping("/product")
+    public ResponseEntity<ProductDTO> createProduct(@RequestBody Product product){
+        return createProductService.execute(product);
     }
 
-
-    @PutMapping
-    public ResponseEntity<String> updateProduct(){
-        return updateProductService.execute(null);
+    @PutMapping("/product/{id}")
+    public ResponseEntity<ProductDTO> updateProduct(@PathVariable Integer id,@RequestBody Product product){
+        return updateProductService.execute(new UpdateProductCommand(id,product));
     }
 
-    @GetMapping
-    public ResponseEntity<String> getProduct(){
-        return getProductService.execute(null);
+    @GetMapping("/products")
+    public ResponseEntity<List<ProductDTO>> getProducts(){
+        return getProductsService.execute(null);
     }
 
+//   Get by ID
+    @GetMapping("/product/{id}")
+    public ResponseEntity<ProductDTO> getProduct(@PathVariable Integer id){
+        return getProductService.execute(id);
+    }
 
-    @DeleteMapping
-    public ResponseEntity<String> deleteProduct(){
-        return deleteProductService.execute(null);
+    @DeleteMapping("/product/{id}")
+    public ResponseEntity<Void> deleteProduct(@PathVariable Integer id){
+        return deleteProductService.execute(id);
     }
 }
