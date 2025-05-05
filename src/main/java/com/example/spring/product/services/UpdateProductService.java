1658f259a75346c859aa0247e4bb5c4869ad1dad
Sam Kikoma <kikomasamson@gmail.com>
2025-05-05 23:13:18 +0200

    feat: Handle product updates and return ProductDTO

--- src/main/java/com/example/spring/product/services/UpdateProductService.java
+++ src/main/java/com/example/spring/product/services/UpdateProductService.java
@@ -1,14 +1,33 @@
-package com.example.spring.product;
+package com.example.spring.product.services;
 
 import com.example.spring.Command;
-import org.springframework.http.HttpStatus;
+import com.example.spring.product.ProductRepository;
+import com.example.spring.product.model.Product;
+import com.example.spring.product.model.ProductDTO;
+import com.example.spring.product.model.UpdateProductCommand;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
-public class UpdateProductService implements Command<Void,String> {
+public class UpdateProductService implements Command<UpdateProductCommand, ProductDTO> {
+
+    private ProductRepository productRepository;
+
+    public UpdateProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
     @Override
-    public ResponseEntity<String> execute(Void input) {
-        return ResponseEntity.status(HttpStatus.OK).body("Product updated");
+    public ResponseEntity<ProductDTO> execute(UpdateProductCommand command) {
+        Optional<Product> productOptional = productRepository.findById(command.getId());
+        if(productOptional.isPresent()){
+            Product product = command.getProduct();
+            product.setId(command.getId());
+            productRepository.save(product);
+            return ResponseEntity.ok(new ProductDTO(product));
+        }
+        return null;
     }
 }
