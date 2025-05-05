ad8dd48d3f01d32583a5fe2b9686036fd0f096b8
Sam Kikoma <kikomasamson@gmail.com>
2025-05-05 23:12:37 +0200

    feat: Get product details by ID

---
+++ src/main/java/com/example/spring/product/services/GetProductService.java
@@ -0,0 +1,29 @@
+package com.example.spring.product.services;
+
+import com.example.spring.Query;
+import com.example.spring.product.ProductRepository;
+import com.example.spring.product.model.Product;
+import com.example.spring.product.model.ProductDTO;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class GetProductService implements Query<Integer, ProductDTO> {
+
+    private final ProductRepository productRepository;
+
+    public GetProductService(ProductRepository productRepository){
+        this.productRepository = productRepository;
+    }
+
+    @Override
+    public ResponseEntity<ProductDTO> execute(Integer input) {
+        Optional<Product> productOptional = productRepository.findById(input);
+        if(productOptional.isPresent()){
+            return ResponseEntity.ok(new ProductDTO(productOptional.get()));
+        }
+        return null;
+    }
+}
