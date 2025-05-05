9f11ade64d3093f6cddcf6e1d69e89884d6a81e2
Sam Kikoma <kikomasamson@gmail.com>
2025-05-05 23:12:16 +0200

    feat: Product deletion by ID

--- src/main/java/com/example/spring/product/services/DeleteProductService.java
+++ src/main/java/com/example/spring/product/services/DeleteProductService.java
@@ -1,14 +1,30 @@
-package com.example.spring.product;
+package com.example.spring.product.services;
 
 import com.example.spring.Command;
+import com.example.spring.product.ProductRepository;
+import com.example.spring.product.model.Product;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
-public class DeleteProductService implements Command<Void,String> {
+public class DeleteProductService implements Command<Integer,Void> {
+
+    private ProductRepository productRepository;
+
+    public DeleteProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
     @Override
-    public ResponseEntity<String> execute(Void input) {
-        return ResponseEntity.status(HttpStatus.NO_CONTENT).body("Product deleted");
+    public ResponseEntity<Void> execute(Integer id) {
+        Optional<Product> productOptional = productRepository.findById(id);
+        if(productOptional.isPresent()){
+            productRepository.deleteById(id);
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+        }
+        return null;
     }
 }
