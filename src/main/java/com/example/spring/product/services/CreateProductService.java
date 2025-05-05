2175c8b4fa1e04d2d54df54c30b7c7c2fbe969ef
Sam Kikoma <kikomasamson@gmail.com>
2025-05-05 23:11:43 +0200

    feat: Update CreateProductService to handle Product creation and return ProductDTO

--- src/main/java/com/example/spring/product/services/CreateProductService.java
+++ src/main/java/com/example/spring/product/services/CreateProductService.java
@@ -1,14 +1,24 @@
-package com.example.spring.product;
+package com.example.spring.product.services;
 
 import com.example.spring.Command;
+import com.example.spring.product.ProductRepository;
+import com.example.spring.product.model.Product;
+import com.example.spring.product.model.ProductDTO;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
-public class CreateProductService implements Command<Void,String> {
+public class CreateProductService implements Command<Product, ProductDTO> {
+    private final ProductRepository productRepository;
+
+    public CreateProductService(ProductRepository productRepository){
+        this.productRepository = productRepository;
+    }
+
     @Override
-    public ResponseEntity<String> execute(Void input) {
-        return ResponseEntity.status(HttpStatus.CREATED).body("Product created");
+    public ResponseEntity<ProductDTO> execute(Product product) {
+        Product savedProduct = productRepository.save(product);
+        return ResponseEntity.status(HttpStatus.CREATED).body(new ProductDTO(savedProduct));
     }
 }
