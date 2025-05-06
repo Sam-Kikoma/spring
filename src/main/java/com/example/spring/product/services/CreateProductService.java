75dccdf30712cc0725c0a8ab7a753cbaa909b962
Sam Kikoma <kikomasamson@gmail.com>
2025-05-06 22:08:48 +0200

    refactor

--- src/main/java/com/example/spring/product/services/CreateProductService.java
+++ src/main/java/com/example/spring/product/services/CreateProductService.java
@@ -1,12 +1,17 @@
 package com.example.spring.product.services;
 
 import com.example.spring.Command;
+import com.example.spring.exceptions.ErrorMessages;
+import com.example.spring.exceptions.ProductNotFoundException;
+import com.example.spring.exceptions.ProductNotValidException;
 import com.example.spring.product.ProductRepository;
 import com.example.spring.product.model.Product;
 import com.example.spring.product.model.ProductDTO;
+import com.example.spring.validators.ProductValidator;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
 
 @Service
 public class CreateProductService implements Command<Product, ProductDTO> {
@@ -18,7 +23,10 @@ public class CreateProductService implements Command<Product, ProductDTO> {
 
     @Override
     public ResponseEntity<ProductDTO> execute(Product product) {
+        //Validation
+//        ProductValidator.execute(product);
         Product savedProduct = productRepository.save(product);
         return ResponseEntity.status(HttpStatus.CREATED).body(new ProductDTO(savedProduct));
     }
+
 }
