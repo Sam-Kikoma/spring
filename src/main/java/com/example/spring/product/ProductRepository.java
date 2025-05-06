65922c13cbc77be50553b8c02043be15a72dbe56
Sam Kikoma <kikomasamson@gmail.com>
2025-05-06 22:06:23 +0200

    feat: Add method to find products by name ignoring case

--- src/main/java/com/example/spring/product/ProductRepository.java
+++ src/main/java/com/example/spring/product/ProductRepository.java
@@ -2,8 +2,18 @@ package com.example.spring.product;
 
 import com.example.spring.product.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product,Integer> {
+    List<Product> findByNameContainingIgnoreCase(String name);
+
+    //JPQL
+    //@Query("SELECT p FROM Product p WHERE p.name LIKE %:keyword% OR p.description LIKE %:keyword%")
+    //List<Product> findByNameOrDescriptionContaining(@Param("keyword") String name);
+
 }
